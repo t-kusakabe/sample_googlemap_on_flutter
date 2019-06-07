261db9cab12fd66981c79c3d890b8a9d561702af
t-kusakabe <ttmac0704@gmail.com>
2019-06-08 08:27:20 +0900

    Modify

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,18 @@ class MapsDemo extends StatefulWidget {
 }
 
 class MapsDemoState extends State<MapsDemo> {
-
   GoogleMapController mapController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ここを追加
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: LatLng(35.6580339, 139.7016358),
+            target: LatLng(35.6580339,139.7016358),
             zoom: 17.0,
           ),
           myLocationEnabled: true,

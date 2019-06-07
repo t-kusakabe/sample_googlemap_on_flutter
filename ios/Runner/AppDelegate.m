1185311e444de6f747a0c477a3a0f3078e70665c
t-kusakabe <ttmac0704@gmail.com>
2019-06-07 22:47:33 +0900

    keyだけ隠しておく

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,8 +6,8 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@""];
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
